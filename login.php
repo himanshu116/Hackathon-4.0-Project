99682cd305b078b41d12991a5b1924df7cd6b5f2
himanshu116 <mlucky.shm116@gmail.com>
2018-03-20 21:11:02 +0530

    Hospital Appointments

---
+++ login.php
@@ -0,0 +1,36 @@
+<!Doctype html>
+<html>
+<head>
+<title> user reg </title>
+<link rel="stylesheet" type= " text/css" href = "style.css" >
+</head>
+<body>
+<div class = "header" >
+<h2> Login </h2>
+</div>
+<form method = "post" action = "login.php">
+<div class = "input-group" >
+<label> username </label>
+<input type = "text" name= "username" >
+ </div>
+ <div class = "input-group" >
+<label> pass </label>
+<input type = "pwd" name= "pass" >
+</div>
+
+<div class = "input-group" >
+<button type = "submit" name ="login" class = "btn"> login </button>
+
+
+
+
+</div>
+<p>
+Not yet a member? <a href = "register.php"> signup </a>
+</p>
+
+
+
+</form>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,45 @@
+<!Doctype html>
+<html>
+<head>
+<title> user reg </title>
+<link rel="stylesheet" type= " text/css" href = "style.css" >
+</head>
+<body>
+<div class = "header" >
+<h2> register </h2>
+</div>
+<form method = "post" action = "register.php">
+<div class = "input-group" >
+<label> username </label>
+<input type = "text" name= "username" >
+ </div>
+ <div class = "input-group" >
+<label> mail </label>
+<input type = "email" name= "email" >
+</div>
+<div class = "input-group" >
+<label> pass </label>
+<input type = "pwd" name= "pass" >
+</div>
+<div class = "input-group" >
+<label> username </label>
+<input type = "text" name= "username" >
+
+
+</div>
+<div class = "input-group" >
+<button type = "submit" name ="register" class = "btn"> register </button>
+
+
+
+
+</div>
+<p>
+Already a member? <a href = "login.php"> signin </a>
+</p>
+
+
+
+</form>
+</body>
+</html>
